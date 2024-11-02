1f1d4968a49e12aa83e769fc985b7fc5be7fb222
scso-ucsc <scso@ucsc.edu>
2024-11-01 20:41:10 -0700

    L16 Dialog - Adding Button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 // todo
+function createButton(): void {
+  const buttonContainer = document.getElementById("button-container");
+  if (buttonContainer === null) {
+    return;
+  }
+
+  const newButton = document.createElement("button");
+  newButton.innerText = "Click to see message!";
+  newButton.addEventListener("click", () => {
+    alert("you clicked the button!");
+  });
+
+  buttonContainer.appendChild(newButton);
+}
+
+globalThis.onload = () => {
+  createButton();
+};
